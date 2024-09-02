02302bb98e0dcae9095bb62607e213b9924fb41c
levabala <levabala@ya.ru>
2024-09-02 17:02:17 +0200

    chore: eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,20 @@
+// @ts-check
+
+import eslint from "@eslint/js";
+import tseslint from "typescript-eslint";
+
+export default tseslint.config(
+    eslint.configs.recommended,
+    ...tseslint.configs.recommended,
+    {
+        
+        languageOptions: {
+            parserOptions: {
+                project: './tsconfig.json',
+            },
+        },
+        rules: {
+            "@typescript-eslint/strict-boolean-expressions": "error",
+        },
+    },
+);
